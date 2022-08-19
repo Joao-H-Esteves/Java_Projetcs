ea7ee2437ccfadbea8953d2022ca4534cce6015f
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-08-19 05:51:48 -0300

    Initial commit

---
+++ Primeiro_Projeto_NetBeans/src/main/java/HoraDoPc/HoraDoSistema.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HoraDoPc;
+
+import java.util.Date;
+
+/**
+ *
+ * @author joao.esteves
+ */
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        //Só digitar psvm que ele preenche sozinho
+        Date data = new Date();
+        System.out.println("A hora atual é ");
+        System.out.println(data.toString());
+    }
+}
+
+        
